47e584d0e81a901fdd8c8a8142d67b064cdbff3a
Joy Christine <joychristin2@gmail.com>
2020-11-16 19:10:46 +0300

    editing

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-
+import tom from '../components/img/tom.jpg';
 export default class Profile extends Component {
     render() {
         return ( 
             <div >
-
+<div className="card">
+    <div className="card-image">
+        <img className="activator "src={tom} alt="them" />
+    </div>
+</div>
             
             </div>
         )
