35c099bb24035e056a77f8b58b3bf7df986852e6
Joy Christine <joychristin2@gmail.com>
2020-11-17 13:28:51 +0300

    edited

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,60 +4,31 @@ export default class Bio extends Component {
     render() {
         return (
             <div>
-                <div class="card-panel teal">
-                  <h5>BIOGRAPHY</h5>
+                <div class="card-panel teal pt-2">
+                  <b>BIOGRAPHY</b> <br></br>
                     <span class="white-text">I am a very simple card. I am good at containing small bits of information.
-                  I am convenient because I require little markup to use effectively.I am a very simple card. I am good at containing small bits of information.
-                    I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                        I am convenient because I require little markup to use effectively.I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
                     </span>
+                    <hr></hr>
                     <div className="card-content">
                         <b>Personal Info</b>
-                        <div class="row">
+                        <div class="bio row">
+                            <div class="col s6">
+                                <p><strong>Address:</strong> 123 Lorem</p>
+                                <p><strong>Email:</strong> test@yahoo.com</p>
+                                <p><strong>Phone:</strong> 123 456 7898</p>
+                            </div>
                         <div class="col s6">
-                        <p>
-                <strong>Address:</strong> 123 Lorem
-              </p>
-              <p>
-                <strong>Email:</strong> test@yahoo.com
-              </p>
-              <p>
-                <strong>Phone:</strong> 123 456 7898
-              </p>
-                        </div>
-                        <div class="col s6">
-                        <p>
-                <strong>Address:</strong> 123 Lorem
-              </p>
-              <p>
-                <strong>Email:</strong> test@yahoo.com
-              </p>
-              <p>
-                <strong>Phone:</strong> 123 456 7898
-              </p>
+                            <p><strong>Address:</strong> 123 Lorem</p>
+                            <p><strong>Email:</strong> test@yahoo.com</p>
+                            <p><strong>Phone:</strong> 123 456 7898</p>
                         </div>
                     </div>
                   </div>
                 </div>
             </div>
-            
-                        /* <div className="row mt2">
-                            <ul className="col s12 m7 l4">
-                            <li>www</li>
-                            <li>www</li>
-                            <li>www</li>
-                            </ul>
-                            <ul className="s12 m7 l4">
-                            <li>www</li>
-                            <li>www</li>
-                            <li>www</li>
-                            </ul>
-                            
-                        </div> */
-                    /* </div>
-               </div> */
-            
-  
-            
+       
         );
     }
 }
