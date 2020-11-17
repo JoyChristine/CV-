5e6877db9260fe3b90a88c91cd1918347bb794f4
Joy Christine <joychristin2@gmail.com>
2020-11-17 12:15:38 +0300

    complete

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,8 +4,49 @@ export default class Skills extends Component {
     render() {
         return (
             <div>
-                
+                <div className="card-panel teal">
+                    <b>SKILLS</b>
+                    <div className="row">
+                        <div class="col s6">
+                            <p>SSS</p>
+                            <div class="progress grey lighten-2">
+                                <div className="determinate light-green" style={{width: '80%'}}></div>
+                            </div>
+
+                            <p>SSS</p>
+                            <div class="progress grey lighten-2">
+                                <div className="determinate light-green" style={{width: '80%'}}></div>
+                            </div>
+
+                            <p>SSS</p>
+                            <div class="progress grey lighten-2">
+                                <div className="determinate light-green" style={{width: '80%'}}></div>
+                            </div>
+                        
+                        </div>
+                        <div class="col s6">
+                            <p>SSS</p>
+                            <div class="progress grey lighten-2">
+                                <div className="determinate light-green" style={{width: '80%'}}></div>
+                            </div>
+
+                            <p>SSS</p>
+                            <div class="progress grey lighten-2">
+                                <div className="determinate light-green" style={{width: '80%'}}></div>
+                            </div>
+
+                            <p>SSS</p>
+                            <div class="progress grey lighten-2">
+                                <div className="determinate light-green" style={{width: '80%'}}></div>
+                            </div>
+
+                        </div>
+                    
+                    </div>
+                </div>
             </div>
-        )
+                    
+                   
+        );
     }
 }
