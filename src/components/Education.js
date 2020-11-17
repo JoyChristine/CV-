7008c0b96edb67d69a23302c7e57c0d8c41270e1
Joy Christine <joychristin2@gmail.com>
2020-11-17 11:21:28 +0300

    details added

--- src/components/Education.js
+++ src/components/Education.js
@@ -4,7 +4,13 @@ export default class Education extends Component {
     render() {
         return (
             <div>
-                
+                <div class="card-panel teal">
+                    <strong>EDUCATION</strong>
+                    <p>School | Degree</p>
+                    <p>Course details</p>
+                    <ol></ol>
+                    <li>Lorem ipsum </li>
+                </div>
             </div>
         )
     }
