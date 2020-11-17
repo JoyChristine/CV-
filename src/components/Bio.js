bf6390f7c7738fdda713016d2ec504e7d7e9fafd
Joy Christine <joychristin2@gmail.com>
2020-11-17 11:06:25 +0300

    bio complete

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,22 +4,60 @@ export default class Bio extends Component {
     render() {
         return (
             <div>
-                 <div className="bio">
-                <div class="card">
-                 <div class="card-image waves-effect waves-block waves-light">
-                    
+                <div class="card-panel teal">
+                  <h5>BIOGRAPHY</h5>
+                    <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+                  I am convenient because I require little markup to use effectively.I am a very simple card. I am good at containing small bits of information.
+                    I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                    </span>
+                    <div className="card-content">
+                        <b>Personal Info</b>
+                        <div class="row">
+                        <div class="col s6">
+                        <p>
+                <strong>Address:</strong> 123 Lorem
+              </p>
+              <p>
+                <strong>Email:</strong> test@yahoo.com
+              </p>
+              <p>
+                <strong>Phone:</strong> 123 456 7898
+              </p>
+                        </div>
+                        <div class="col s6">
+                        <p>
+                <strong>Address:</strong> 123 Lorem
+              </p>
+              <p>
+                <strong>Email:</strong> test@yahoo.com
+              </p>
+              <p>
+                <strong>Phone:</strong> 123 456 7898
+              </p>
+                        </div>
+                    </div>
                   </div>
-    <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
-    </div>
-    <div class="card-reveal">
-      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-      <p>Here is some more information about this product that is only revealed once clicked on.</p>
-    </div>
-  </div>
                 </div>
             </div>
-        )
+            
+                        /* <div className="row mt2">
+                            <ul className="col s12 m7 l4">
+                            <li>www</li>
+                            <li>www</li>
+                            <li>www</li>
+                            </ul>
+                            <ul className="s12 m7 l4">
+                            <li>www</li>
+                            <li>www</li>
+                            <li>www</li>
+                            </ul>
+                            
+                        </div> */
+                    /* </div>
+               </div> */
+            
+  
+            
+        );
     }
 }
