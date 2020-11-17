17376d3d81920b965e93e760f3e82f9cdf83adbd
Joy Christine <joychristin2@gmail.com>
2020-11-17 12:30:55 +0300

    experience details added

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react'
 
 export default class Experience extends Component {
     render() {
-        return (
-            <div>
-                
-            </div>
-        )
+        return ( 
+            <div > 
+                <div className="card-panel teal">
+                        <b>Experience</b>
+                    <div className="row">
+                        <div class="col s4">
+                            <p>Position</p>
+                            <span className="ex-co">Company | duration   
+                            </span>
+                        <ol></ol>
+                            <li>Lorem ipsum </li>  
+                        </div>
+                        <div class="col s4">
+                            <p>Position</p>
+                            <span className="ex-co">Company | duration   
+                            </span>
+                        <ol></ol>
+                            <li>Lorem ipsum </li>  
+                        </div>
+                        <div class="col s4">
+                            <p>Position</p>
+                            <span className="ex-co">Company | duration   
+                            </span>
+                        <ol></ol>
+                            <li>Lorem ipsum </li>  
+                        </div>
+                   </div>
+                    
+                </div>
+            </div> 
+            
+        );
     }
 }
