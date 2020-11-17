f836d88ede144364de0206982bd4edb57c9cf6a3
Joy Christine <joychristin2@gmail.com>
2020-11-17 13:28:03 +0300

    initial commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react'
 import tom from '../components/img/tom.jpg';
+import css from '../App.css';
+
+
 export default class Projects extends Component {
     render() {
         return ( 
         <div>
-            <div className="card-panel">
+            <div className="card-panel teal">
+                <b>PROJECTS</b>
+                <br></br>
             <div class="row ">
-                <div class="col s4">
-                <img className="activator"src={tom} style={{width: '90%'}} alt="Profilepic" />
+                <div class="projects col s4">
+                <img className="projects"src={tom} style={{width: '90%'}} alt="Profilepic" />
                 <span className="dark-text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio voluptatum quas totam ipsum sit placeat ipsa saepe asperiores culpa reiciendis quaerat, qui veniam commodi fugit. Laboriosam nisi incidunt aliquam fuga.</span>
                 </div>
-                <div class="col s4">
+                <div class="projects col s4">
                 <img className="activator"src={tom} style={{width: '90%'}}alt="Profilepic" />
                 <span className="black-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit quidem quos optio, dolore provident in quam architecto voluptas nisi iure reiciendis excepturi vel cupiditate nam quia aperiam odit unde debitis.</span>
                 </div>
-                <div class="col s4">
+                <div class="projects col s4">
                 <img className="activator"src={tom} style={{width: '90%'}}alt="Profilepic" />
                 <span className="dark-text ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, itaque praesentium officiis harum eum odio vitae fugiat delectus culpa eos cumque ut, odit labore eaque fuga amet ipsa, repudiandae quos.</span>
                 </div>
             </div>
+            
             </div>
            
 
